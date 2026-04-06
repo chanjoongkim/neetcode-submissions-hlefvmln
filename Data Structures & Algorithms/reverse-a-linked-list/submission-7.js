a05e46ffa065c0fb4900297494e31b50295aa7fe
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-06 10:55:25 -0500

    Add: reverse-a-linked-list - submission-7

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-7.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head) {
+        let curr = head;
+        let follow = null;
+
+        while (curr !== null) {
+            const next = curr.next;
+
+            curr.next = follow;
+            follow = curr;
+            curr = next;
+        }
+
+        return follow;
+    }
+}
