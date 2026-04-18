06d528269da6bd3455f7c61619442214663273a2
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-18 14:19:46 -0500

    Add: merge-two-sorted-linked-lists - submission-16

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-16.js
@@ -0,0 +1,79 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} list1
+     * @param {ListNode} list2
+     * @return {ListNode}
+     */
+    mergeTwoLists(list1, list2) {
+        if (list1 === null && list2 === null) {
+            return null;
+        } else if (list1 === null) {
+            return list2;
+        } else if (list2 === null) {
+            return list1;
+        }
+
+        // return this.mergeTwoListsRecursive(list1, list2);
+
+        // iterative approach
+        let dummy = new ListNode();
+        let tail = dummy;
+
+        while (list1 !== null && list2 !== null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (list1 === null) {
+            tail.next = list2;
+        }
+        if (list2 === null) {
+            tail.next = list1;
+        }
+
+        return dummy.next;
+    }
+
+    mergeTwoListsRecursive(list1, list2) {
+        if (list1 === null && list2 === null) {
+            return null;
+        } else if (list1 === null) {
+            return list2;
+        } else if (list2 === null) {
+            return list1;
+        }
+
+        // return the smaller of the two elements
+        const head = list1.val > list2.val ? list2 : list1;
+
+        if (list1.val > list2.val) {
+            const nextL2 = list2.next;
+
+            // since list2 is smaller, recursively set list2.next to the next smallest element (that is returned from our recursive call)
+            list2.next = this.mergeTwoListsRecursive(list1, nextL2);
+        } else {
+            const nextL1 = list1.next;
+
+            // since list1 is smaller, recursively set list1.next to the next smallest element (that is returned from our recursive call)
+            list1.next = this.mergeTwoListsRecursive(nextL1, list2);
+        }
+
+        return head;
+    }
+}
