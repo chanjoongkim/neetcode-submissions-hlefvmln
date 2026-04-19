c1d779a6450437ba2ee5848c20d23b622aa8eb28
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-19 14:54:44 -0500

    Add: longest-repeating-substring-with-replacement - submission-20

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-20.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        if (!s) {
+            return 0;
+        }
+        if (s.length === 1) {
+            return 1;
+        }
+
+        const charFreq = new Map();
+        let left = 0;
+        // charFreq.set(s.charAt(0), 1);
+        let right = 0;
+        let maxLength = 0;
+
+        while (right < s.length) {
+            const c = s.charAt(right);
+            let freq = charFreq.get(c) ?? 0;
+            freq++;
+
+            charFreq.set(c, freq);
+            const maxFreq = Math.max(...charFreq.values());
+            const replacements = (right - left + 1) - maxFreq;
+
+            if (replacements > k) {
+                // need to move left over by one and update the freqs
+                const oldLeftChar = s.charAt(left);
+                left++;
+                charFreq.set(oldLeftChar, charFreq.get(oldLeftChar) - 1);
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+            right++;
+        }
+
+        return maxLength;
+    }
+}
