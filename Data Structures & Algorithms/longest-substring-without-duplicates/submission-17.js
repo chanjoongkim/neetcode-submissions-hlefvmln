088f23e5cfdb57c60c7b809f9f88b4586f2ad29b
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-19 14:11:14 -0500

    Add: longest-substring-without-duplicates - submission-17

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-17.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        if (!s) {
+            return 0;
+        }
+        if (s.length === 1) {
+            return 1;
+        }
+
+        const charsIndexMap = new Map();
+        charsIndexMap.set(s.charAt(0), 0);
+
+        let maxLength = 1;
+        let left = 0;
+
+        for (let i = 1; i < s.length; i++) {
+            const char = s.charAt(i);
+            if (!charsIndexMap.has(char)) {
+                charsIndexMap.set(char, i);
+                
+            }
+            else {
+                // if we detect a duplicate char, then we find the value of the old index of char
+                // and reset the bound of left to be one past that, then update the index value of char
+                const oldIndex = charsIndexMap.get(char);
+                // verify that the old index is to the right of our current left. Otherwise if it's an earlier index
+                // then we don't need to reset left (since our current left is already past the old index)
+                left = Math.max(left, oldIndex + 1);
+                charsIndexMap.set(char, i);
+            }
+            maxLength = Math.max(maxLength, i - left + 1);
+        }
+
+        return maxLength;
+    }
+}
