3ba7939e6e27dd79495792d8060867cf6ad610e2
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-20 14:08:53 -0500

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.js
@@ -0,0 +1,53 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const result = [];
+        const charsMap = new Map();
+
+
+        for (const str of strs) {
+            // build up chars
+            const strChars = this.buildCharFreqs(str);
+
+            // go through result and see if we already have a list containing anagrams that match str
+            if (charsMap.has(strChars)) {
+                const anagramList = charsMap.get(strChars);
+                anagramList.push(str);
+                charsMap.get(strChars, anagramList);
+            }
+            else {
+                charsMap.set(strChars, [str]);
+            }
+        }
+
+        return [...charsMap.values()];
+    }
+
+    buildCharFreqs(str) {
+        const charFreqs = new Array(26).fill(0);
+
+        for (const c of str) {
+            charFreqs[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+        }
+
+        const key = charFreqs.join(',');
+        return key;
+    }
+
+    charFreqsEqual(sChars, tChars) {
+        if (sChars.size !== tChars.size) {
+            return false;
+        }
+
+        for (const [sKey, sValue] of sChars) {
+            if (!tChars.has(sKey) || tChars.get(sKey) !== sValue) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
