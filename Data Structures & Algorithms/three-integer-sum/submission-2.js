10d028c1cfb799040185aad06f51e5e5d08bed05
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-21 10:26:08 -0500

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.js
@@ -0,0 +1,55 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const map = new Map();
+
+        nums.sort((a, b) => a - b);
+
+        // iterate through nums and map all values to indices that contain it
+        for (let i = 0; i < nums.length; i++) {
+            const num = nums[i];
+            let indices = null;
+            if (map.has(num)) {
+                indices = map.get(num);
+            } else {
+                indices = new Set();
+            }
+
+            indices.add(i);
+            map.set(num, indices);
+        }
+
+        console.log(nums);
+
+        // list of sets initially, convert set to arrays at the end
+        const resultSet = new Set();
+
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                const sum = nums[i] + nums[j];
+                const complement = 0 - sum;
+                if (map.has(complement)) {
+                    const indices = map.get(complement);
+                    for (const index of indices) {
+                        if (index === i || index === j || index < j) {
+                            continue;
+                        }
+                        // if indices contains more indices than i/j, then we have a valid triplet
+                        resultSet.add([nums[i], nums[j], complement].join(','));
+                    }
+                }
+            }
+        }
+
+        console.log(resultSet);
+        let result = [];
+        for (const triple of resultSet) {
+            result.push(triple.split(','));
+        }
+
+        return result;
+    }
+}
