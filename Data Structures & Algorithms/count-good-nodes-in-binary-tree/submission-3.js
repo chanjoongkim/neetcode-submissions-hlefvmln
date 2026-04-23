8eb92d8170e09de5589871bfc21cfbb8440e2362
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-23 09:50:45 -0500

    Add: count-good-nodes-in-binary-tree - submission-3

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-3.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    goodNodes(root) {
+        if (!root) {
+            return 0;
+        }
+
+        // root always counts as a good node
+        this.result = 0;
+
+        this.dfs(root, root.val);
+
+        return this.result;
+    }
+
+    dfs(node, max) {
+        // do a pre-order traversal of the tree
+        // but pass along the "max" seen in the path so far.
+        // if the current node is greater than max, then increment this.result
+        // continue recursing the tree, and update the max we send if the current node.val is greater than max
+
+        if (!node) {
+            return;
+        }
+
+        if (node.val >= max) {
+            this.result++;
+            max = node.val;
+        }
+        this.dfs(node.left, max);
+        this.dfs(node.right, max);
+    }
+}
