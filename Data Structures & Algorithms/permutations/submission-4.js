5476338bf8ae1b2ebea7df63d37ede8be7c6fd05
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-23 23:07:09 -0500

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permute(nums) {
+        if (!nums) {
+            return [];
+        }
+
+        const curr = [];
+        const result = [];
+        const used = new Array(nums.length).fill(false);
+
+        this.permuteHelper(nums, used, curr, result);
+
+        return result;
+    }
+
+    permuteHelper(nums, used, curr, result) {
+        if (curr.length === nums.length) {
+            result.push([...curr]);
+            return;
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                const num = nums[i];
+                curr.push(num);
+                this.permuteHelper(nums, used, curr, result);
+                curr.pop();
+                used[i] = false;
+            }
+        }
+
+        // const num = nums.pop();
+        // curr.push(num);
+        // this.permuteHelper(nums, curr, result);
+        // curr.pop();
+        // nums.push(num);
+    }
+}
