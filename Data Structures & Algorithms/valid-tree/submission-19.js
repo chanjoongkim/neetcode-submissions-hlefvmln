7b185d0fb0b745817fc344c76419a7ca2740ac86
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-26 13:30:58 -0500

    Add: valid-tree - submission-19

---
+++ Data Structures & Algorithms/valid-tree/submission-19.js
@@ -0,0 +1,63 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        // build up tree with Nodes, then see if there is a cycle within the graph
+        const graph = new Map();
+
+        for (let i = 0; i < n; i++) {
+            graph.set(i, new Set());
+        }
+
+        for (const [a, b] of edges) {
+            const aNeighbors = graph.get(a);
+            const bNeighbors = graph.get(b);
+
+            aNeighbors.add(b);
+            bNeighbors.add(a);
+
+            graph.set(a, aNeighbors);
+            graph.set(b, bNeighbors);
+        }
+
+
+        return !this.hasCycle(graph)
+    }
+
+    hasCycle(graph) {
+        // pick node 0, then do a BFS from there, and if we encounter a node we've already visited then return false
+        // else at the end our visited set size should equal the number of nodes
+
+        const visited = new Set();
+        const queue = new Queue();
+
+        queue.enqueue([0, -1]);
+
+        while (!queue.isEmpty()) {
+            const [node, parent] = queue.dequeue();
+            visited.add(node);
+            
+            // go through neighbors
+            const neighbors = graph.get(node);
+
+            for (const neighbor of neighbors) {
+                // skip current node (the parent)
+                if (neighbor === parent) {
+                    continue;
+                }
+
+                // found a cycle, so return true (since we have a cycle)
+                if (visited.has(neighbor)) {
+                    return true;
+                }
+
+                queue.enqueue([neighbor, node]);
+            }
+        }
+
+        return visited.size !== graph.size;
+    }
+}
