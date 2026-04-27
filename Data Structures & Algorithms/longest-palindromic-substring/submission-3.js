e05863f0161870f4e733207339127d488f3f5884
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-27 13:28:38 -0500

    Add: longest-palindromic-substring - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-3.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    longestPalindrome(s) {
+        if (!s) {
+            return '';
+        }
+
+        let result = '';
+        for (let i = 0; i < s.length; i++) {
+            const oddP = this.getPalindrome(s, i, true);
+            if (oddP.length > result.length) {
+                result = oddP;
+            }
+            const evenP = this.getPalindrome(s, i, false);
+            if (evenP.length > result.length) {
+                result = evenP;
+            }
+        }
+
+        return result;
+    }
+
+    getPalindrome(s, index, isOdd) {
+        let left = index;
+        let right = isOdd ? index : index + 1;
+        
+        let result = '';
+
+        while (left >= 0 && right < s.length && s.at(left) === s.at(right)) {
+            result = s.substring(left, right + 1);
+            left--;
+            right++;
+        }
+
+        return result;
+    } 
+}
