7e308490cbb059dab0da843ec22a951985746ea2
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-26 22:54:33 -0500

    Add: house-robber - submission-8

---
+++ Data Structures & Algorithms/house-robber/submission-8.js
@@ -0,0 +1,56 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums) {
+        if (!nums) {
+            return 0;
+        }
+        if (nums.length < 2) {
+            return nums[0];
+        }
+
+        let twoHouses = nums[nums.length - 1];
+        let oneHouse = nums[nums.length - 2];
+
+        for (let i = nums.length - 3; i >= 0; i--) {
+            const house = Math.max(nums[i] + twoHouses, oneHouse);
+            twoHouses = oneHouse;
+            oneHouse = house;
+        }
+
+        return Math.max(oneHouse, twoHouses);
+        
+        // extra space
+        const houses = new Array(nums.length).fill(0);
+
+        houses[nums.length - 1] = nums[nums.length - 1];
+        houses[nums.length - 2] = nums[nums.length - 2];
+
+        for (let i = nums.length - 3; i >= 0; i--) {
+            houses[i] = Math.max(nums[i] + houses[i + 2], houses[i + 1]);
+        }
+
+        return Math.max(houses[0], houses[1]);
+
+        // recursive with memo
+        const memo = new Map();
+
+        return this.robMemo(nums, 0, memo);
+    }
+
+    robMemo(nums, i, memo) {
+        if (memo.has(i)) {
+            return memo.get(i);
+        }
+        if (i >= nums.length) {
+            return 0;
+        }
+
+        const max = Math.max(nums[i] + this.robMemo(nums, i + 2, memo), this.robMemo(nums, i + 1, memo));
+        memo.set(i, max);
+
+        return max;
+    }
+}
