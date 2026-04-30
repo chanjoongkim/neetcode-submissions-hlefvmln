09f95c6410c635c8835b927f6237098019c655f6
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-30 14:06:09 -0500

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    jump(nums) {
+        if (!nums) {
+            return 0;
+        }
+
+        let result = 0;
+        let index = 0;
+
+        while (index < nums.length - 1) {
+            const num = nums[index];
+
+            if (index + num >= nums.length - 1) {
+                result++;
+                break;
+            }
+
+            // greedily find the next "max jump" spot we can reach
+            let maxJump = 0;
+            let nextIndex = -1;
+            for (let i = 1; i <= num; i++) {
+                if (index + i + nums[index + i] >= maxJump) {
+                    maxJump = index + i + nums[index + i];
+                    nextIndex = index + i;
+                }
+            }
+
+            result++;
+            index = nextIndex;
+        }
+
+        return result;
+    }
+}
