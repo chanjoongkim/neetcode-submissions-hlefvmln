03865a6c16bf1bf7c5331d3ec45085cc6bb226e2
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-04-30 14:38:10 -0500

    Add: hand-of-straights - submission-2

---
+++ Data Structures & Algorithms/hand-of-straights/submission-2.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} hand
+     * @param {number} groupSize
+     * @return {boolean}
+     */
+    isNStraightHand(hand, groupSize) {
+        hand.sort((a, b) => a - b);
+
+        // greedily try to make straights of groupSize, starting at the smallest
+        // remove elements from our array/hand as we build up straights
+        
+        while (hand.length > 0) {
+            console.log(hand);
+            let num = hand[0];
+            const straight = new Set();
+            straight.add(num);
+            hand.shift();
+            // build up straight
+            let i = 0;
+            while (straight.size < groupSize && i < hand.length) {
+                if (hand[i] === num + 1) {
+                    num = hand[i];
+                    straight.add(num);
+                    hand.splice(i, 1);
+                } else {
+                    i++;
+                }
+            }
+
+            if (straight.size !== groupSize) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
