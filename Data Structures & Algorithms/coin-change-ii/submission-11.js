3049b404d3fa98695ed74232f74d36e0586f9bec
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-03 14:17:08 -0500

    Add: coin-change-ii - submission-11

---
+++ Data Structures & Algorithms/coin-change-ii/submission-11.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number} amount
+     * @param {number[]} coins
+     * @return {number}
+     */
+    change(amount, coins) {
+        const memo = new Map();
+        return this.dfs(coins, 0, amount, memo);
+    }
+
+    dfs(coins, index, amount, memo) {
+        if (amount === 0) {
+            return 1;
+        }
+
+        if (amount < 0 || index >= coins.length) {
+            return 0;
+        }
+
+        const key = index + '-' + amount;
+
+        if (memo.has(key)) {
+            return memo.get(key);
+        }
+
+        let result = 0;
+
+        // either use the coin and re-use the same index
+        // or skip this coin denomination altogether and move to the next index
+        result += this.dfs(coins, index, amount - coins[index], memo);
+        result += this.dfs(coins, index + 1, amount, memo);
+
+        // for (let i = index; i < coins.length; i++) {
+        //     result += this.dfs(coins, i, amount - coins[i], memo);
+        // }
+
+        memo.set(key, result);
+
+        return result;
+    }
+}
