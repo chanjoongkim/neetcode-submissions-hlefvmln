fd2ef3cae8874987278c37ad873a7c7fc97bf8ca
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-03 14:14:10 -0500

    Add: coin-change-ii - submission-7

---
+++ Data Structures & Algorithms/coin-change-ii/submission-7.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number} amount
+     * @param {number[]} coins
+     * @return {number}
+     */
+    change(amount, coins) {
+        const memo = new Map();
+        return this.dfs(coins, 0, amount, memo);
+    }
+
+    dfs(coins, index, amount, memo) {
+        if (amount === 0) {
+            return 1;
+        }
+
+        if (amount < 0 || index >= coins.length) {
+            return 0;
+        }
+
+        const key = index + '-' + amount;
+
+        if (memo.has(key)) {
+            return memo.get(key);
+        }
+
+        let result = 0;
+
+        for (let i = index; i < coins.length; i++) {
+            result += this.dfs(coins, i, amount - coins[i], memo);
+        }
+
+        memo.set(key, result);
+
+        return result;
+    }
+}
