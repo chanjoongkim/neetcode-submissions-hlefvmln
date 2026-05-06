9b411004cef4ec47f0c1b5a9c17983574ab72492
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-06 10:49:13 -0500

    Add: non-overlapping-intervals - submission-16

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-16.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number}
+     */
+    eraseOverlapIntervals(intervals) {
+        if (!intervals) {
+            return [];
+        }
+
+        // sort by start time
+        intervals.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+
+        const result = [];
+        result.push(intervals[0]);
+
+        console.log(intervals);
+
+        for (let i = 1; i < intervals.length; i++) {
+            const interval = intervals[i];
+
+            // if they overlap, greedily pick the interval with the smaller end time
+            if (this.overlaps(result.at(-1), interval)) {
+                if (result.at(-1)[1] > interval[1]) {
+                    // replace last result interval with current interval
+                    result[result.length - 1] = interval;
+                }
+            }
+            else {
+                result.push(interval);
+            }
+        }
+
+        return intervals.length - result.length;
+    }
+
+    overlaps(interval1, interval2) {
+        // we only need to check if the start of the second interval is smaller than the first interval's end
+        return interval2[0] < interval1[1];
+        // return (interval1[0] < interval2[0] && interval2[0] <= interval1[1]) || 
+        // (interval2[0] < interval1[0] && interval1[0] <= interval2[1]);
+    }
+}
