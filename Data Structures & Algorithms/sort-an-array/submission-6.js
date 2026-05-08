3d3ecea86802edfedf62e89ba46ae5b2763f62a9
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-07 20:32:25 -0500

    Add: sort-an-array - submission-6

---
+++ Data Structures & Algorithms/sort-an-array/submission-6.js
@@ -0,0 +1,77 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortArray(nums) {
+        this.sort(nums, 0, nums.length - 1);
+
+        return nums;
+    }
+
+    sort(nums, left, right) {
+        console.log(left, right);
+        if (left >= right) {
+            return;
+        }
+
+        // divide nums into left/right half
+        // sort the left/right halves recursively,
+        // then merge them together
+
+        let mid = Math.floor((left + right) / 2);
+
+        this.sort(nums, left, mid);
+        this.sort(nums, mid + 1, right);
+
+        // const left = this.sort([...nums.slice(0, mid)]);
+        // const right = this.sort([...nums.slice(mid)]);
+
+        this.merge(nums, left, mid, right);
+    }
+
+    merge(nums, leftI, midI, rightI) {
+        const left = [];
+        const right = [];
+
+        for (let i = leftI; i <= midI; i++) {
+            left.push(nums[i]);
+        }
+
+        for (let i = midI + 1; i <= rightI; i++) {
+            right.push(nums[i]);
+        }
+
+
+        // const result = new Array(left.length + right.length).fill(0);
+
+        let leftIndex = 0;
+        let rightIndex = 0;
+
+        let index = leftI;
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex] < right[rightIndex]) {
+                nums[index] = left[leftIndex];
+                leftIndex++;
+            }
+            else {
+                nums[index] = right[rightIndex];
+                rightIndex++
+            }
+            index++;
+        }
+
+        while (leftIndex < left.length) {
+            nums[index] = left[leftIndex];
+            leftIndex++;
+            index++;
+        }
+        while (rightIndex < right.length) {
+            nums[index] = right[rightIndex];
+            rightIndex++;
+            index++;
+        } 
+
+        // return result;
+    }
+}
