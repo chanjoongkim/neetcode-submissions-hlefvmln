c2f18f29453b7ef47566c846518c0cb254062251
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-10 14:19:52 -0500

    Add: asteroid-collision - submission-13

---
+++ Data Structures & Algorithms/asteroid-collision/submission-13.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[]} asteroids
+     * @return {number[]}
+     */
+    asteroidCollision(asteroids) {
+        // use stack to keep track of asteroids we encounter that are rightwards
+        // when we see a leftward one, we compare it with the top of the stack (since that's the first asteroid it will meet)
+
+        const stack = [];
+
+        for (const asteroid of asteroids) {
+            stack.push(asteroid);
+            this.handleCollisions(stack);
+        }
+
+        return stack;
+    }
+
+    handleCollisions(stack) {
+        while (stack.length > 1) {
+            const lastAsteroid = stack.pop();
+            // no more collisions (same directions)
+            // OR top of stack is going left (negative) and lastAsteroid is going right (positive)
+            if ((lastAsteroid < 0 && stack.at(-1) < 0) || (lastAsteroid > 0 && stack.at(-1) > 0) || (stack.at(-1) < 0 && lastAsteroid > 0)) {
+                stack.push(lastAsteroid);
+                return;
+            }
+            // collision
+            if (Math.abs(lastAsteroid) === Math.abs(stack.at(-1))) {
+                stack.pop();
+            }
+            // remove top of stack and push lastAsteroid
+            else if (Math.abs(lastAsteroid) > Math.abs(stack.at(-1))) {
+                stack.pop();
+                stack.push(lastAsteroid);
+            }
+            // lastAsteroid should not be in stack, which already occurred so continue
+            else {
+                continue;
+            }
+        }
+    }
+}
