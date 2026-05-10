ffec4a2c6a742ef9e0ac210f401bb2a153573917
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-10 15:05:52 -0500

    Add: simplify-path - submission-2

---
+++ Data Structures & Algorithms/simplify-path/submission-2.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} path
+     * @return {string}
+     */
+    simplifyPath(path) {
+        if (!path || path.at(0) !== '/') {
+            return '';
+        } 
+
+        const items = path.split('/');
+        const stack = [];
+        for (const item of items) {
+            // if item = '', ignore because this means it was just one or more slashes
+            if (item === '') {
+                continue;
+            }
+
+            if (item === '.') {
+                // ignore if it's single dot meaning current directory as it's a no-op
+                continue;
+            }
+            else if (item === '..') {
+                // we should pop the last item from the stack
+                stack.pop();
+            }
+            // directory/file name, so add to stack
+            else {
+                stack.push(item);
+            }
+        }
+
+        // build result string from stack
+        return '/' + stack.join('/');
+    }
+}
