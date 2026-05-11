9a88736d1c32ae5eeed1e4ce314d3599e2a8d67d
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-11 18:57:01 -0500

    Add: minimum-size-subarray-sum - submission-6

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-6.js
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} nums
+     * @return {number}
+     */
+    minSubArrayLen(target, nums) {
+        // algorithm:
+        // use sliding window, build initial window until window sum >= target
+        // if sum === target, then record length and see if it's the smallest
+        // else shrink window until we are <= target
+        // once again, if === target then record
+        // other once we are < target, then we go right until we exceed target and repeat
+
+        let l = 0;
+        let curr = 0
+        let result = Number.MAX_SAFE_INTEGER;
+
+        // go through all nums
+        for (let r = 0; r < nums.length; r++) {
+            curr += nums[r];
+
+            // if curr sum is < target, then just keep adding nums[r]
+            if (curr < target) {
+                continue;
+            }
+
+            // once our curr sum is >= target, we shrink our window until we no longer are >= target
+            while (l <= r && curr >= target) {
+                // always check if our window size is shortest
+                // result = Math.min(r - l + 1, result);
+                curr -= nums[l];
+                l++;
+            }
+            // we only need to check if the last window size was our best
+            result = Math.min(r - l + 2, result);
+        }
+
+        return result === Number.MAX_SAFE_INTEGER ? 0 : result;
+    }
+}
