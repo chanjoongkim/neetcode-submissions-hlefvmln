9f9257ee457c2648ae62be17845616f09351f340
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-13 16:33:08 -0500

    Add: maximum-frequency-stack - submission-7

---
+++ Data Structures & Algorithms/maximum-frequency-stack/submission-7.js
@@ -0,0 +1,58 @@
+class FreqStack {
+    constructor() {
+        this.freqs = new Map();
+        this.freqsLevel = new Map();
+        this.maxFreq = 0;
+
+        // special case to handle 0 frequency
+        this.freqsLevel.set(0, []);
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        let freq = this.freqs.get(val) ?? 0;
+        
+        freq++;
+
+        if (this.freqsLevel.has(freq)) {
+            this.freqsLevel.get(freq).push(val);
+        }
+        else {
+            this.freqsLevel.set(freq, [val]);
+        }
+
+        if (freq > this.maxFreq) {
+            this.maxFreq = freq;
+        }
+
+        this.freqs.set(val, freq);
+    }
+
+    /**
+     * @return {number}
+     */
+    pop() {
+        // get number from max freq
+        const result = this.freqsLevel.get(this.maxFreq).pop();
+
+        let freq = this.freqs.get(result);
+
+        this.freqs.set(result, freq - 1);
+
+        while (this.freqsLevel.get(this.maxFreq).length === 0 && this.maxFreq > 0) {
+            this.maxFreq--;
+        }
+
+        return result;
+    }
+}
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * var obj = new FreqStack()
+ * obj.push(val)
+ * var param_2 = obj.pop()
+ */
