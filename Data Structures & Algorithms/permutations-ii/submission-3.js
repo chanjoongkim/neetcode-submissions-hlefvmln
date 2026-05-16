552aca817e58801c7c9d35d19fb8b1474745b801
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-16 15:26:21 -0500

    Add: permutations-ii - submission-3

---
+++ Data Structures & Algorithms/permutations-ii/submission-3.js
@@ -0,0 +1,55 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    permuteUnique(nums) {
+        nums.sort((a, b) => a - b);
+
+        const result = [];
+        const curr = [];
+
+        this.backtrack(nums, curr, result);
+
+        return result;
+    }
+
+    backtrack(nums, curr, result) {
+        if (curr.length === nums.length) {
+            result.push([...curr]);
+            return;
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            // skip if already used in the current path
+            if (nums[i] === -Infinity) {
+                continue;
+            }
+
+            // skip duplicates, if this isn't the first time we see this number at this depth AND 
+            // the previous one is available to be used
+            if (i > 0 && nums[i] === nums[i - 1] && nums[i - 1] !== -Infinity) {
+                continue;
+            }
+
+
+            const num = nums[i];
+            // add nums[i] to curr and remove nums[i] from nums, then recurse for each index
+            // const newNums = [...nums.slice(0, i), ...nums.slice(i + 1)];
+            nums[i] = -Infinity
+            curr.push(num);
+
+            this.backtrack(nums, curr, result);
+
+            curr.pop();
+            nums[i] = num;
+
+            // // skip duplicates
+            // while (i + 1 < nums.length && nums[i + 1] !== -Infinity && nums[i] === nums[i + 1]) {
+            //     i++;
+            // }
+            // i++;
+            
+        }
+    }
+}
