0af7cb49dc55e90548ec9be6e3b14a8b5203d33c
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-17 15:06:34 -0500

    Add: n-th-tribonacci-number - submission-3

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-3.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    tribonacci(n) {
+        if (n < 3) {
+            if (n === 0) {
+                return 0;
+            }
+            else {
+                return 1;
+            }
+        }
+
+        let t0 = 0;
+        let t1 = 1;
+        let t2 = 1;
+
+        let i = 3;
+        while (i <= n) {
+            const temp = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = temp;
+            i++;
+        }
+
+        return t2;
+    }
+}
