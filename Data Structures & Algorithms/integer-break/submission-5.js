3a0d27a6f9465c8ac5d73f92f048529c61c9e3f1
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-18 11:46:20 -0500

    Add: integer-break - submission-5

---
+++ Data Structures & Algorithms/integer-break/submission-5.js
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    integerBreak(n) {
+        const memo = new Map();
+        const result = this.getSums(n, 0, memo);
+
+        return result;
+
+        // return this.getMaxProduct(result);
+    }
+
+    getSums(n, currSum, memo) {
+        if (currSum === n) {
+            return 1;
+        }
+
+        if (memo.has(currSum)) {
+            return memo.get(currSum);
+        }
+
+        // contains all sets of numbers that sum up to currSum
+        let result = 0;
+
+        for (let i = 1; i <= n - 1; i++) {
+            if (currSum + i <= n) {
+                const remainderProduct = this.getSums(n, currSum + i, memo);
+                result = Math.max(result, i * remainderProduct);
+                // for (const remainder of remainders) {
+                //     const nums = 
+                //     result.push([i, ...remainder]);
+                // }
+            }
+        }
+
+        memo.set(currSum, result);
+
+        return result;
+    }
+
+    getMaxProduct(sums) {
+        let max = 0;
+        for (const sum of sums) {
+            const product = sum.reduce((acc, curr) => acc * curr, 1);
+            max = Math.max(max, product);
+        }
+
+        return max;
+    }
+}
