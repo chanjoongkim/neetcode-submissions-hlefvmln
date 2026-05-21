f6fce9d9673ed98e1b627178034ba43fcd7f5628
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-21 10:36:50 -0500

    Add: maximum-sum-circular-subarray - submission-12

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-12.js
@@ -0,0 +1,61 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubarraySumCircular(nums) {
+        if (!nums) {
+            return 0;
+        }
+
+        const min = Math.min(...nums);
+
+        let maxSum = min < 0 ? min : 0;
+        let index = 0;
+        let curr = 0;
+
+        while (index < nums.length) {
+            curr += nums[index];
+
+            maxSum = Math.max(curr, maxSum);
+
+            if (curr < 0) {
+                curr = 0;
+            }
+            index++;
+        }
+
+        if (maxSum === 0) {
+            maxSum = min;
+        }
+
+        let minSum = 0;
+        curr = 0;
+        index = 0;
+
+        while (index < nums.length) {
+            curr -= nums[index];
+
+            minSum = Math.max(curr, minSum);
+
+            if (curr < 0) {
+                curr = 0;
+            }
+            index++;
+        }
+
+        minSum *= -1;
+
+        console.log(maxSum, minSum);
+
+        const totalSum = nums.reduce((acc, curr) => acc + curr, 0);
+
+        // edge case, if minSum === totalSum then all numbers are negative
+        // in which case we should just return maxSum (which will be the least negative number)
+        if (totalSum === minSum) {
+            return maxSum;
+        }
+
+        return Math.max(maxSum, totalSum - minSum);
+    }
+}
