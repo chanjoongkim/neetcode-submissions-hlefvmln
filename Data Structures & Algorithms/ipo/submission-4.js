a2848f954c8b9504887854bc3f66a4802320d1cc
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-20 21:52:44 -0500

    Add: ipo - submission-4

---
+++ Data Structures & Algorithms/ipo/submission-4.js
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {number} k
+     * @param {number} w
+     * @param {number[]} profits
+     * @param {number[]} capital
+     * @return {number}
+     */
+    findMaximizedCapital(k, w, profits, capital) {
+        // new array of [profit, capital] pairs
+        const projects = [];
+
+        for (let i = 0; i < profits.length; i++) {
+            projects.push([profits[i], capital[i]]);
+        }
+
+        // sort projects based on capital required
+        projects.sort((a, b) => a[1] - b[1]);
+
+        // priority queue where we prioritize profits
+        const pq = new MaxPriorityQueue((p) => p[0]);
+
+        let currentCapital = w;
+        let index = 0;
+
+        while (k > 0 && index < projects.length) {
+            if (projects[index][1] > currentCapital) {
+                return currentCapital;
+            }
+            // add projects we have enough capital for to our priority queue
+            while (index < projects.length && projects[index][1] <= currentCapital) {
+                pq.push(projects[index]);
+                index++;
+            }
+
+            // pick our most profitable project with our given capital
+            if (!pq.isEmpty()) {
+                const project = pq.pop();
+                currentCapital += project[0];
+                k--;
+            }
+        }
+
+        while (!pq.isEmpty() && k > 0) {
+            const project = pq.pop();
+            currentCapital += project[0];
+            k--;
+        }
+
+        return currentCapital;
+    }
+}
