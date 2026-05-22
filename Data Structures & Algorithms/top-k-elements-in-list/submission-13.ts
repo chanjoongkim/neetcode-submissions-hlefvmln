c050be02d17d5090fba9699e0294c2737d1c6dd6
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-22 09:55:04 -0500

    Add: top-k-elements-in-list - submission-13

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-13.ts
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        if (!nums) {
+            return [];
+        }
+
+        const freqs = new Map();
+        for (const num of nums) {
+            freqs.set(num, (freqs.get(num) ?? 0) + 1);
+        }
+
+        const buckets = Array.from({ length: nums.length + 1 }, () => []);
+        for (const [num, freq] of freqs) {
+            const freqBucket = buckets[freq];
+            freqBucket.push(num);
+        }
+
+        const result = [];
+        let index = buckets.length - 1;
+        // assume that k fits into bucket divisions cleanly
+        while (index >= 0 && k > 0) {
+            const mostFreq = buckets[index];
+            for (const num of mostFreq) {
+                result.push(num);
+                k--;
+
+                // break early if k doesn't fit into buckets cleanly
+                if (k === 0) {
+                    return result;
+                }
+            }
+            index--;
+        }
+
+        return result;
+    }
+}
