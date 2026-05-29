c7b0e30a8c12a3af59bfd4f5db21a6873a9b3387
CJ Kim <chanjoong.cj.kim@gmail.com>
2026-05-29 16:45:33 -0500

    Add: stone-game - submission-2

---
+++ Data Structures & Algorithms/stone-game/submission-2.ts
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @return {boolean}
+     */
+    stoneGame(piles: number[]): boolean {
+        const memo = new Map();
+        const result = this.dfs(piles, 0, piles.length - 1, memo);
+
+        return result > 0 ? true : false;
+    }
+
+    dfs(piles: number[], left: number, right: number, memo: Map<string, number>): number {
+        if (left > right) {
+            return 0;
+        }
+
+        if (left === right) {
+            return piles[left];
+        }
+
+        const key = left + '-' + right;
+
+        if (memo.has(key)) {
+            return memo.get(key);
+        }
+
+        // at each step, we take the largest gap between taking the pile at either end and the other player's turn
+        // ultimately if the final number is positive, Alice won. Else Bob won.
+        const takeLeft = piles[left] - this.dfs(piles, left + 1, right, memo);
+        const takeRight = piles[right] - this.dfs(piles, left, right - 1, memo);
+
+        memo.set(key, Math.max(takeLeft, takeRight));
+
+        return memo.get(key);
+    }
+}
